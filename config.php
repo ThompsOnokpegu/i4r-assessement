4436def3ffe6b05d015532d53c8bc8b2f9dae40f
AJThompson <tommyriode@gmail.com>
2023-05-14 13:16:23 +0100

    Fixed DB User missing Error

--- config.php
+++ config.php
@@ -4,12 +4,15 @@
   * Configuration for database connection
   *
   */
+  $username   = "u130540156_i4r00sdb";
   $password   = "i4.0r@S2023!";
   $dbname     = "u130540156_i4r00s";
-  $host       = "localhost";
-  $username   = "root";
+  
+  //$username   = "root";
   //$password   = "";
   //$dbname     = "product_readiness_upc_spdc";
+
+  $host       = "localhost";
   $dsn        = "mysql:host=$host;dbname=$dbname";
   $options    = array(
                   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
